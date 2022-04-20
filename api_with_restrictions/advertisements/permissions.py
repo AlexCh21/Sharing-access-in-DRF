f611c2fa826978995c87028a0d8a9a70a4f26746
Alexandr Ch <90438795+AlexCh21@users.noreply.github.com>
2022-04-20 20:06:49 +1100

    Update permissions.py

--- api_with_restrictions/advertisements/permissions.py
+++ api_with_restrictions/advertisements/permissions.py
@@ -1,13 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwnerOrViewOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
+
         if request.method in SAFE_METHODS:
             return True
         return obj.creator == request.user
-
 
 class IsAdminUserOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
