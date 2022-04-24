cae183b110567c8d7c36366c6ea8a42574f6760e
Alexandr Ch <90438795+AlexCh21@users.noreply.github.com>
2022-04-24 12:16:35 +1100

    Update views.py

--- api_with_restrictions/advertisements/views.py
+++ api_with_restrictions/advertisements/views.py
@@ -1,40 +1,29 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from .permissions import IsOwnerOrReadOnly
-
-from .filters import AdvertisementFilter
-from .models import Advertisement
-from .serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly, IsAdminUserOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filter_class = AdvertisementFilter
-    filterset_fields = ['created_at', 'updated_at', 'status','creator']
-    search_fields = ['creator']
+    filterset_fields = ['created_at', 'updated_at', 'status', 'creator']
     ordering_fields = ['created_at', 'updated_at', 'status']
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly | IsAdminUserOrReadOnly]
 
-    
-
-    def get_queryset(self):
-        param = self.request.query_params.get('creator')
-        if param:
-            return Advertisement.objects.filter(creator=param)
-    
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+            return [IsAuthenticated(), IsOwnerOrReadOnly(), IsAdminUserOrReadOnly()]
         return []
-
-
 
     
 
