05f2393c11200ae820042cb1f130d05336019811
Alexandr Ch <90438795+AlexCh21@users.noreply.github.com>
2022-04-24 12:20:22 +1100

    Update filters.py

--- api_with_restrictions/advertisements/filters.py
+++ api_with_restrictions/advertisements/filters.py
@@ -1,13 +1,15 @@
-from django_filters import rest_framework as filters, DateFromToRangeFilter
+from django_filters import rest_framework as filters
+from django_filters import FilterSet
 
 from .models import Advertisement
 
 
-class AdvertisementFilter(filters.FilterSet):
+class AdvertisementFilter(FilterSet):
     """Фильтры для объявлений."""
 
-    created_at = DateFromToRangeFilter()
+    created_at = filters.DateFromToRangeFilter()
+    update_at = filters.DateFromToRangeFilter()
 
     class Meta:
         model = Advertisement
-        fields = ['created_at']
+        fields = ['created_at', 'creator', 'status']
