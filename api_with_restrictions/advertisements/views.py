0794c5a9a2f3d600fb6844da894bb468d92e20de
Alexandr Ch <90438795+AlexCh21@users.noreply.github.com>
2022-04-27 10:44:37 +1100

    Update views.py

--- api_with_restrictions/advertisements/views.py
+++ api_with_restrictions/advertisements/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend 
-from rest_framework.filters import SearchFilter, OrderingFilter
+# from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework.viewsets import ModelViewSet 
 
@@ -15,11 +15,11 @@ class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter] #
+    filter_backends = [DjangoFilterBackend] # SearchFilter, OrderingFilter
     filter_class = AdvertisementFilter #
     filterset_fields = ['created_at', 'updated_at', 'status','creator']
-    search_fields = ['creator']
-    ordering_fields = ['created_at', 'updated_at', 'status']
+    # search_fields = ['creator']
+    # ordering_fields = ['created_at', 'updated_at', 'status']
     
 
     def get_queryset(self):
